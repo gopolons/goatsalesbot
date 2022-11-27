989df82a583bed3a275683acad8981c03c31f9bf
Georgii Polonskii <gopolons@gmail.com>
2022-11-27 21:50:30 +0000

    minor update

--- Utility/Data/AppDataRepository.py
+++ Utility/Data/AppDataRepository.py
@@ -3,6 +3,8 @@ from .DS.DatabaseConnector import DatabaseConnector
 from .DS.SampleDataInstance import SampleDataInstance
 from ..Model.Classes.Order import Order
 from typing import List
+from Log.Logging import Logging
+
 
 class AppDataRepository:
     dbInterface = any
@@ -26,9 +28,14 @@ class AppDataRepository:
         return self.dbInterface.storePlaceOrder(order)
 
     def __init__(self):
+        self.logger = Logging.instance().logger
+        self.logger.debug("Initilialising data repository")
         if debug:
+
+            self.logger.debug("Running in debug")
             self.dbInterface = SampleDataInstance()
             return
         else:
+            self.logger.debug("Running on live data")
             self.dbInterface = DatabaseConnector()
             return
--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from Managers.SessionManager import SessionManager
 from Log.Logging import Logging
 
 logger = Logging.instance()
-logger.logger.debug("___GOSHANOTE___")
+logger.logger.debug("__GOATSALES2022__")
 logger.logger.debug("APP STARTED")
 
 session = SessionManager("5914085904:AAEe1Pyu9DlT4SHWKIeVamoJ4yj65Mxbrok")
